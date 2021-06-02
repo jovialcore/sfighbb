6c93418d06b8cc1457c9ac772a62d1ed000aa2c6
SunnyTechie <sunnyict001@gmail.com>
2021-06-02 16:01:28 -0700

    Update for 6/2/2021 user.php under update

--- app/Models/User.php
+++ app/Models/User.php
@@ -50,7 +50,7 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
-
+/*
     protected static function boot() {
         parent::boot();
 
@@ -59,4 +59,5 @@ class User extends Authenticatable
         });
         
     }
+*/
 }
